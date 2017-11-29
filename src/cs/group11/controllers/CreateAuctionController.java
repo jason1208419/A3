4323e98172e7a50b2793fe838f199487a580e170
fillpant <fillpant@gmx.co.uk>
2017-11-29 20:56:54 +0000

    Begun the implementation of CreateAuctionController.

--- src/cs/group11/controllers/CreateAuctionController.java
+++ src/cs/group11/controllers/CreateAuctionController.java
@@ -1,50 +1,78 @@
 package cs.group11.controllers;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
-
 import javafx.scene.control.Button;
-
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
-
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
 
-import javafx.scene.image.ImageView;
-
-import javafx.scene.control.RadioButton;
-
 public class CreateAuctionController {
+
 	@FXML
 	private ImageView image;
+
 	@FXML
 	private RadioButton sculptureRadio;
+
 	@FXML
 	private RadioButton paintingRadio;
+
 	@FXML
 	private TextField title;
+
 	@FXML
 	private TextField auction;
+
 	@FXML
 	private TextField startPrice;
+
 	@FXML
 	private TextField width;
+
 	@FXML
 	private TextField length;
+
 	@FXML
 	private TextField creationDate;
+
 	@FXML
 	private FlowPane depthPane;
+
 	@FXML
 	private TextField depth;
+
 	@FXML
 	private FlowPane materialPane;
+
 	@FXML
 	private TextField material;
+
 	@FXML
 	private Button createButton;
 
 	@FXML
 	protected void initialize() {
-		
+		ToggleGroup radioGroup = new ToggleGroup();
+		sculptureRadio.setToggleGroup(radioGroup);
+		paintingRadio.setToggleGroup(radioGroup);
+
+		radioGroup.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Toggle> observable, Toggle oldValue, Toggle newValue) {
+				if (observable.getValue() != null && observable.getValue().equals(sculptureRadio)) {// TODO test
+					boolean show = newValue.isSelected();
+					materialPane.setVisible(show);
+					depthPane.setVisible(show);
+				}
+			}
+		});
+
 	}
 
 }
