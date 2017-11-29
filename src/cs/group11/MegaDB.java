2b81fec0e4a4a0f16db1c61ae61aeeb1179ac499
fillpant <fillpant@gmx.co.uk>
2017-11-29 22:14:33 +0200

    Created MegaDB with the basic, raw methods. More methods to be added, and further implementation&validation required for existing methods.

---
+++ src/cs/group11/MegaDB.java
@@ -0,0 +1,58 @@
+package cs.group11;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import cs.group11.models.Artwork;
+import cs.group11.models.Auction;
+import cs.group11.models.User;
+
+public final class MegaDB {
+
+	private static List<Auction> auctions = new LinkedList<>();
+	private static List<Artwork> artworks = new ArrayList<>();
+	private static Set<User> users = new HashSet<>();
+
+	private MegaDB() {
+	}// Prevent instantiation of class.
+
+	public static void load() {
+		// TODO implement once FileReader is in place.
+	}
+
+	public static void save() {
+		// TODO implement once FileReader is in place.
+	}
+
+	public static Collection<Auction> getAuctions() {
+		return auctions;
+	}
+
+	public static Collection<User> getUsers() {
+		return users;
+	}
+
+	public static Collection<Artwork> getArtworks() {
+		return artworks;
+	}
+
+	public static boolean addAuction(Auction toAdd) {
+		toAdd.validate();
+		return auctions.add(toAdd);
+	}
+
+	public static boolean addUser(User toAdd) {
+		toAdd.validate();
+		return users.add(toAdd);
+	}
+
+	public static boolean addArtwork(Artwork toAdd) {
+		toAdd.validate();
+		return artworks.add(toAdd);
+	}
+
+}
