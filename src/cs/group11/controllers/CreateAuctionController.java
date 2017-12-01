7dced9ea9418c812a966fdb457aeda2dc66f8d0e
fillpant <fillpant@gmx.co.uk>
2017-12-01 01:23:13 +0000

    Introduced createAuctionDefaultIcon.png used in the CreateAuction GUI as the default image, Implemented ImageView for CreateAuctionController but lacking critical error correction and notification features that will be added in future commits.

--- src/cs/group11/controllers/CreateAuctionController.java
+++ src/cs/group11/controllers/CreateAuctionController.java
@@ -1,22 +1,34 @@
 package cs.group11.controllers;
 
+import java.io.File;
+
+import cs.group11.helpers.Validator;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 
+/**
+ * @author Filippos Pantekis
+ */
 public class CreateAuctionController {
+
+	private static final Image NO_IMAGE_SELECTED_ICON;
+	static {
+		NO_IMAGE_SELECTED_ICON = new Image(ClassLoader.getSystemResourceAsStream("res/createAuctionDefaultIcon.png"));
+	}
 
 	@FXML
 	private BorderPane mainPane;
@@ -65,6 +77,7 @@ public class CreateAuctionController {
 
 	@FXML
 	protected void initialize() {
+		// Setup the toggle group for the radiobuttons.
 		ToggleGroup radioGroup = new ToggleGroup();
 		sculptureRadio.setToggleGroup(radioGroup);
 		paintingRadio.setToggleGroup(radioGroup);
@@ -72,7 +85,29 @@ public class CreateAuctionController {
 			materialPane.setVisible(newState);
 			depthPane.setVisible(newState);
 		});
+
+		// Set the image
+		image.setImage(NO_IMAGE_SELECTED_ICON);
+		image.setOnMouseClicked((e) -> {
+			Image selected = userSelectImage();
+			if (Validator.isNull(selected)) {
+				// TODO show alert
+			} else
+				image.setImage(selected);
+		});
+
 		
+	}
+
+	
+	private Image userSelectImage() {
+		FileChooser chooser = new FileChooser();
+		chooser.setSelectedExtensionFilter(new ExtensionFilter("Image Files", ".png", ".gif", ".jpeg", ".jpg"));
+		chooser.setTitle("Select Image");
+		File in = chooser.showOpenDialog(null);
+		if (!Validator.isFileValid(in))
+			return null;
+		return new Image(in.toURI().toString());
 	}
 
 	// THIS WILL BE DELETED! JUST FOR TESTING PURPOSES!
