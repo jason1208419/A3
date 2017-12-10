e037db2d26acc9439217cf928c91610dde8e5dde
Thomas <908356@swansea.ac.uk>
2017-12-10 17:09:06 +0000

    Javadocs completed for User

--- src/cs/group11/models/User.java
+++ src/cs/group11/models/User.java
@@ -12,6 +12,10 @@ import cs.group11.helpers.InvalidDataException;
 import cs.group11.helpers.Validator;
 import cs.group11.interfaces.Validatable;
 
+/**
+ * A user of the system
+ * @author Nasir
+ */
 public class User implements Validatable, Serializable {
 	private static final long serialVersionUID = 1L;
 
@@ -30,6 +34,15 @@ public class User implements Validatable, Serializable {
 	private List<Bid> bids;
 	private List<Auction> createdAuctions;
 
+	/**
+	 * Constructs a user.
+	 * @param username The username of the user.
+	 * @param firstname The first name of the user.
+	 * @param lastname The last name of the user.
+	 * @param telNo The telephone number of the user.
+	 * @param address The address of the user.
+	 * @param avatarPath The filepath of the user's avatar
+	 */
 	public User(String username, String firstname, String lastname, String telNo, Address address, String avatarPath) {
 		this.id = getNextId();
 
@@ -50,6 +63,10 @@ public class User implements Validatable, Serializable {
 		MegaDB.addUser(this);
 	}
 
+	/**
+	 * Assigns the user a unique id.
+	 * @return The user's id to be asssigned.
+	 */
 	private static int getNextId() {
 		List<User> users = MegaDB.getUsers();
 
@@ -61,6 +78,17 @@ public class User implements Validatable, Serializable {
 		return lastUser.getId() + 1;
 	}
 
+	/**
+	 * Constructs a user with an id.
+	 * @param id The unique id of the user
+	 * @param lastLogin The date the user last logged in.
+	 * @param username The username of the user.
+	 * @param firstname The first name of the user.
+	 * @param lastname The last name of the user.
+	 * @param telNo The telephone number of the user.
+	 * @param address The address of the user.
+	 * @param avatarPath The avatar of the user.
+	 */
 	public User(int id, Date lastLogin, String username, String firstname, String lastname, String telNo,
 			Address address, String avatarPath) {
 		this.id = id;
@@ -82,106 +110,195 @@ public class User implements Validatable, Serializable {
 		MegaDB.addUser(this);
 	}
 
+	/**
+	 * @return Returns the id of the user.
+	 */
 	public int getId() {
 		return id;
 	}
 
+	/**
+	 * @return Returns the last time the user logged in.
+	 */
 	public Date getLastLogin() {
 		return lastLogin;
 	}
 
+	/**
+	 * Sets the date the user last logged in.
+	 * @param lastLogin the date the user last logged in.
+	 */
 	public void setLastLogin(Date lastLogin) {
 		this.lastLogin = lastLogin;
 	}
 
+	/**
+	 * @return Returns the username of the user.
+	 */
 	public String getUsername() {
 		return username;
 	}
 
+	/**
+	 * Sets the username of the user.
+	 * @param username The username of the user.
+	 */
 	public void setUsername(String username) {
 		this.username = username;
 	}
 
+	/**
+	 * @return Returns the first name of the user.
+	 */
 	public String getFirstname() {
 		return firstname;
 	}
 
+	/**
+	 * Sets the first name of the user.
+	 * @param firstname The first name of the user.
+	 */
 	public void setFirstname(String firstname) {
 		this.firstname = firstname;
 	}
 
+	/**
+	 * @return Returns the last name of the user.
+	 */
 	public String getLastname() {
 		return lastname;
 	}
 
+	/**
+	 * Sets the last name of the user.
+	 * @param lastname The last name of the user.
+	 */
 	public void setLastname(String lastname) {
 		this.lastname = lastname;
 	}
 
+	/**
+	 * @return Returns the user's telephone number
+	 */
 	public String getTelNo() {
 		return telNo;
 	}
 
+	/**
+	 * Sets the telephone number of the user.
+	 * @param telNo The user's telephone number.
+	 */
 	public void setTelNo(String telNo) {
 		this.telNo = telNo;
 	}
 
+	/**
+	 * @return Returns the user's address.
+	 */
 	public Address getAddress() {
 		return address;
 	}
 
+	/**
+	 * @return Returns the path of the user's avatar.
+	 */
 	public String getAvatarPath() {
 		return avatarPath;
 	}
 
+	/**
+	 * Sets the path of the user's avatar.
+	 * @param avatarPath The avatar path of the user.
+	 */
 	public void setAvatarPath(String avatarPath) {
 		this.avatarPath = avatarPath;
 	}
 
+	/**
+	 * @return Returns the user's favourite auctions.
+	 */
 	public List<Auction> getFavouriteAuctions() {
 		return favouriteAuctions;
 	}
 
+	/**
+	 * @return Returns the logged in user's favourite users.
+	 */
 	public List<User> getFavouriteUsers() {
 		return favouriteUsers;
 	}
 
+	/**
+	 * Adds a group of users to the user's favourite users.
+	 * @param users The users to be added.
+	 */
 	public void addAllFavouriteUsers(List<User> users) {
 		this.favouriteUsers.addAll(users);
 	}
 
+	/**
+	 * Adds a group of auctions to the user's favourite auctions.
+	 * @param auctions The auctions to be added.
+	 */
 	public void addAllFavouriteAuctions(List<Auction> auctions) {
 		this.favouriteAuctions.addAll(auctions);
 	}
 
+	/**
+	 * Adds a favourite user to the logged in user's favourite users.
+	 * @param user The user to be added.
+	 */
 	public void addFavouriteUser(User user) {
 		this.favouriteUsers.add(user);
 	}
 
+	/**
+	 * Removes a favourite user from the logged in user's favourite users.
+	 * @param user The user to be removed.
+	 */
 	public void removeFavouriteUser(User user) {
 		this.favouriteUsers.remove(user);
 	}
 
+	/**
+	 * Adds a favourite auction to the logged in user's favourite auctions.
+	 * @param auction The auction to be added.
+	 */
 	public void addFavouriteAuction(Auction auction) {
 		this.favouriteAuctions.add(auction);
 	}
 
+	/**
+	 * Removes a favourite auction from the logged in user's favourite auctions.
+	 * @param auction The auction to be removed.
+	 */
 	public void removeFavouriteAuction(Auction auction) {
 		this.favouriteAuctions.remove(auction);
 	}
 
+	/**
+	 * Stores a new bid placed by the user.
+	 * @param bid The bid to be stored.
+	 */
 	public void addBid(Bid bid) {
 		if (!this.bids.contains(bid)) {
 			this.bids.add(bid);
 		}
 	}
 
+	/**
+	 * Stores a new auction created by the user..
+	 * @param auction The auction to be stored.
+	 */
 	public void addCreatedAuction(Auction auction) {
 		if (!this.createdAuctions.contains(auction)) {
 			this.createdAuctions.add(auction);
 		}
 	}
 
+	/**
+	 * @return Returns all the bids that won the user an auction.
+	 */
 	public List<Bid> getWonBids() {
 		List<Bid> wonBids = new ArrayList<>();
 
@@ -196,6 +313,9 @@ public class User implements Validatable, Serializable {
 		return wonBids;
 	}
 
+	/**
+	 * @return Return the list of bids people have placed on a user's auction.
+	 */
 	public List<Bid> getReceivedBids() {
 		List<Bid> received = new ArrayList<>();
 
@@ -206,14 +326,23 @@ public class User implements Validatable, Serializable {
 		return received;
 	}
 
+	/**
+	 * @return Returns the list of bids the user has placed.
+	 */
 	public List<Bid> getBids() {
 		return bids;
 	}
 
+	/**
+	 * @return Returns the list of auctions the user has created.
+	 */
 	public List<Auction> getCreatedAuctions() {
 		return createdAuctions;
 	}
 
+	/**
+	 * @return Returns a user's information formatted into a Csv storable format.
+	 */
 	public String toCsv() {
 		StringBuilder builder = new StringBuilder();
 
@@ -248,6 +377,10 @@ public class User implements Validatable, Serializable {
 		return str;
 	}
 
+	/**
+	 * Writes information stored in the system to the relevant files.
+	 * @throws IOException Thrown if there are any errors writing to the file.
+	 */
 	public void save() throws IOException {
 		MegaDB.save();
 	}
