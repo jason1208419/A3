240526c3e6c99fe15d5273bbb226b5aff23c4640
Jason <jasonleekinwah@hotmail.com>
2017-12-10 14:57:58 +0000

    JavaDoc

--- src/cs/group11/models/Auction.java
+++ src/cs/group11/models/Auction.java
@@ -9,172 +9,248 @@ import cs.group11.helpers.InvalidDataException;
 import cs.group11.helpers.Validator;
 import cs.group11.interfaces.Validatable;
 
+/**
+ * This class represent an auction storing any necessary information of an auction
+ *
+ * @Author Nasir
+ */
 public class Auction implements Validatable {
 
-	private int id;
+    private int id;
 
-	private User creator;
-	private int maxBids;
-	private Artwork artwork;
-	private double reservePrice;
+    private User creator;
+    private int maxBids;
+    private Artwork artwork;
+    private double reservePrice;
 
-	private List<Bid> bids;
-	private Date creationDate;
+    //list of bids made by users of this auction
+    private List<Bid> bids;
+    private Date creationDate;
 
-	public Auction(User creator, int maxBids, double reservePrice, Artwork artwork) {
-		this.id = getNextId();
+    /**
+     * To initialize an auction
+     * @param creator the creator of the auction
+     * @param maxBids the maximum amount of bids that can be made by users of the auction
+     * @param reservePrice The staring price of the auction
+     * @param artwork The artwork for bidding of the auction
+     */
+    public Auction(User creator, int maxBids, double reservePrice, Artwork artwork) {
+        this.id = getNextId();
 
-		this.creationDate = new Date();
+        this.creationDate = new Date();
 
-		this.creator = creator;
-		this.maxBids = maxBids;
-		this.reservePrice = reservePrice;
-		this.artwork = artwork;
+        this.creator = creator;
+        this.maxBids = maxBids;
+        this.reservePrice = reservePrice;
+        this.artwork = artwork;
 
-		this.validate();
+        this.validate();
 
-		this.bids = new ArrayList<>();
-		creator.addCreatedAuction(this);
+        this.bids = new ArrayList<>();
+        creator.addCreatedAuction(this);
 
-		MegaDB.addAuction(this);
-	}
+        MegaDB.addAuction(this);
+    }
 
-	private static int getNextId() {
-		List<Auction> auctions = MegaDB.getAuctions();
+    /**
+     * To initialize an auction
+     * @param id the unique id of the auction
+     * @param creationDate the date and time of creation of the auction
+     * @param creator the creator of the auction
+     * @param maxBids the maximum amount of bids that can be made by users of the auction
+     * @param reservePrice The staring price of the auction
+     * @param artwork The artwork for bidding of the auction
+     */
+    public Auction(int id, Date creationDate, User creator, int maxBids, double reservePrice, Artwork artwork) {
+        this.id = id;
+        this.creationDate = creationDate;
 
-		if (auctions.size() == 0) {
-			return 0;
-		}
+        this.creator = creator;
+        this.maxBids = maxBids;
+        this.reservePrice = reservePrice;
+        this.artwork = artwork;
 
-		Auction lastAuction = auctions.get(auctions.size() - 1);
-		return lastAuction.getId() + 1;
-	}
+        this.validate();
 
-	public Auction(int id, Date creationDate, User creator, int maxBids, double reservePrice, Artwork artwork) {
-		this.id = id;
-		this.creationDate = creationDate;
+        this.bids = new ArrayList<>();
+        creator.addCreatedAuction(this);
 
-		this.creator = creator;
-		this.maxBids = maxBids;
-		this.reservePrice = reservePrice;
-		this.artwork = artwork;
+        MegaDB.addAuction(this);
+    }
 
-		this.validate();
+    /**
+     * Create an unique id for the auction
+     *
+     * @return the unique id
+     */
+    private static int getNextId() {
+        List<Auction> auctions = MegaDB.getAuctions();
 
-		this.bids = new ArrayList<>();
-		creator.addCreatedAuction(this);
+        //if there is no auction, assign id 0 to the auction
+        if (auctions.size() == 0) {
+            return 0;
+        }
 
-		MegaDB.addAuction(this);
-	}
+        Auction lastAuction = auctions.get(auctions.size() - 1);
+        return lastAuction.getId() + 1;
+    }
 
-	public User getCreator() {
-		return creator;
-	}
+    /**
+     * Get the creator of this auction
+     * @return the creator of this auction
+     */
+    public User getCreator() {
+        return creator;
+    }
 
-	public Artwork getArtwork() {
-		return artwork;
-	}
+    /**
+     * Get the artwork of this auction
+     * @return the artwork of this auction
+     */
+    public Artwork getArtwork() {
+        return artwork;
+    }
 
-	public int getMaxBids() {
-		return maxBids;
-	}
+    /**
+     * Get the maximum amount of bid that can be made by users of this auction
+     * @return the maximum amount of bid
+     */
+    public int getMaxBids() {
+        return maxBids;
+    }
 
-	public List<Bid> getBids() {
-		return bids;
-	}
+    /**
+     * Get a list of bids that have been made by users of this auction
+     * @return a list of bids
+     */
+    public List<Bid> getBids() {
+        return bids;
+    }
 
-	public Bid getLastBid() {
-		if (bids.size() == 0)
-			return null;
-		return bids.get(bids.size() - 1);// top element = last bid.
-	}
+    /**
+     * Get the last bid made of this auction
+     * @return the last bid made
+     */
+    public Bid getLastBid() {
+        if (bids.size() == 0)
+            return null;
+        return bids.get(bids.size() - 1);// top element = last bid.
+    }
 
-	public void addBid(Bid bid) {
-		if (!this.bids.contains(bid)) {
-			this.bids.add(bid);
-		}
-	}
+    /**
+     * Add a bid to the bid list
+     * @param bid the bid to be added
+     */
+    public void addBid(Bid bid) {
+        if (!this.bids.contains(bid)) {
+            this.bids.add(bid);
+        }
+    }
 
-	public void addAllBids(List<Bid> bids) {
-		for (Bid bid : bids) {
-			this.addBid(bid);
-		}
-	}
+    /**
+     * replace with a list of bids to this auction
+     * @param bids the list of bids to be added
+     */
+    public void addAllBids(List<Bid> bids) {
+        for (Bid bid : bids) {
+            this.addBid(bid);
+        }
+    }
 
-	public int getId() {
-		return id;
-	}
+    /**
+     * Get the auction id
+     * @return the id of this auction
+     */
+    public int getId() {
+        return id;
+    }
 
-	public Date getCreationDate() {
-		return creationDate;
-	}
+    /**
+     * Get the creation date of this auction
+     * @return the creation date of this auction
+     */
+    public Date getCreationDate() {
+        return creationDate;
+    }
 
-	public double getReservePrice() {
-		return reservePrice;
-	}
+    /**
+     * Get the staring price of this auction
+     * @return the starting price of this auction
+     */
+    public double getReservePrice() {
+        return reservePrice;
+    }
 
-	public boolean isCompleted() {
-		return this.bids.size() == this.maxBids;
-	}
+    /**
+     * Check if this auction completed
+     * @return true if completed; false otherwise
+     */
+    public boolean isCompleted() {
+        return this.bids.size() == this.maxBids;
+    }
 
-	public String toCsv() {
-		StringBuilder builder = new StringBuilder();
+    /**
+     * Convert auction into a storable format
+     * @return the converted format
+     */
+    public String toCsv() {
+        StringBuilder builder = new StringBuilder();
 
-		builder.append(this.id).append(",");
-		builder.append(this.creationDate.getTime()).append(",");
-		builder.append(this.creator.getId()).append(",");
-		builder.append(this.maxBids).append(",");
-		builder.append(Double.toString(this.reservePrice)).append(",");
-		builder.append(this.artwork.getId()).append(",");
+        builder.append(this.id).append(",");
+        builder.append(this.creationDate.getTime()).append(",");
+        builder.append(this.creator.getId()).append(",");
+        builder.append(this.maxBids).append(",");
+        builder.append(Double.toString(this.reservePrice)).append(",");
+        builder.append(this.artwork.getId()).append(",");
 
-		return builder.toString();
-	}
+        return builder.toString();
+    }
 
-	@Override
-	public void validate() throws InvalidDataException {
-		if (Validator.isNull(artwork)) {
-			throw new InvalidDataException("No artwork specified for auction!");
-		}
-		artwork.validate();
-		if (Validator.isNull(creator)) {
-			throw new InvalidDataException("No creator specified for auction!");
-		}
-		if (!Validator.isIntValid(maxBids, Integer.MAX_VALUE, 1)) {
-			throw new InvalidDataException("Invalid number of maximum bids! Value must be >= 1");
-		}
-		if (Validator.isNegative(reservePrice)) {
-			throw new InvalidDataException("Negative reserve price not permited. Reserve price must be >= 0");
-		}
-	}
+    @Override
+    public void validate() throws InvalidDataException {
+        if (Validator.isNull(artwork)) {
+            throw new InvalidDataException("No artwork specified for auction!");
+        }
+        artwork.validate();
+        if (Validator.isNull(creator)) {
+            throw new InvalidDataException("No creator specified for auction!");
+        }
+        if (!Validator.isIntValid(maxBids, Integer.MAX_VALUE, 1)) {
+            throw new InvalidDataException("Invalid number of maximum bids! Value must be >= 1");
+        }
+        if (Validator.isNegative(reservePrice)) {
+            throw new InvalidDataException("Negative reserve price not permited. Reserve price must be >= 0");
+        }
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (obj == null) {
-			return false;
-		}
-		if (this == obj) {
-			return true;
-		}
-		if (!(obj instanceof Auction)) {
-			return false;
-		}
-		Auction other = (Auction) obj;
-		if (!artwork.equals(other.artwork)) {
-			return false;
-		}
-		if (!creationDate.equals(other.creationDate)) {
-			return false;
-		}
-		if (!creator.equals(other.creator)) {
-			return false;
-		}
-		if (maxBids != other.maxBids) {
-			return false;
-		}
-		if ((int) reservePrice != (int) other.reservePrice) {
-			return false;
-		}
-		return true;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Auction)) {
+            return false;
+        }
+        Auction other = (Auction) obj;
+        if (!artwork.equals(other.artwork)) {
+            return false;
+        }
+        if (!creationDate.equals(other.creationDate)) {
+            return false;
+        }
+        if (!creator.equals(other.creator)) {
+            return false;
+        }
+        if (maxBids != other.maxBids) {
+            return false;
+        }
+        if ((int) reservePrice != (int) other.reservePrice) {
+            return false;
+        }
+        return true;
+    }
 
 }
